4be430a205737697a95fd5956d2445817b8f6cdf
Amin Comp <amin4000malik@gmail.com>
2025-11-06 21:34:08 +0300

    ✅ first page done

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,23 @@
+// vite.config.ts
 import { defineConfig } from "vite";
+import { fileURLToPath, URL } from "node:url";
+import { resolve } from "node:path";
 
 export default defineConfig({
   resolve: {
     alias: {
-      "@": "/src", // теперь @ указывает на папку src
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        // по умолчанию это не нужно, но если страниц несколько — указываем явно
+        main: resolve(__dirname, "index.html"),
+        catalog: resolve(__dirname, "catalog.html"),
+        // пример второй страницы:
+        // admin: resolve(__dirname, 'admin/index.html'),
+      },
     },
   },
 });
