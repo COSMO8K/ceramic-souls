19bb1ab192af6fb1e2641c2b0012003acb04bcf5
Amin Comp <amin4000malik@gmail.com>
2025-11-08 09:12:09 +0300

    🐛 finish the catalog

--- src/js/main.js
+++ src/js/main.js
@@ -27,6 +27,8 @@ close.addEventListener("click", () => {
   document.body.style.overflow = "";
 });
 
+// swiper
+
 try {
   const swiper = new Swiper(".works__slider", {
     slidesPerView: 1,
@@ -54,3 +56,27 @@ try {
     modules: [Navigation, Pagination],
   });
 } catch (e) {}
+
+// tabs
+
+try {
+  const tabs = document.querySelectorAll(".catalog__tab");
+  const contents = document.querySelectorAll(".catalog__content-item");
+
+  tabs.forEach((tab, index) => {
+    tab.addEventListener("click", () => {
+      // Удаляем активный класс у всех табов и контента
+      tabs.forEach((t) => t.classList.remove("catalog__tab_active"));
+      contents.forEach((c) => (c.style.display = "none"));
+
+      // Добавляем активный класс к нажатому табу и показываем соответствующий контент
+      tab.classList.add("catalog__tab_active");
+      contents[index].style.display = "flex";
+    });
+  });
+
+  // Показываем первый контент при загрузке
+  contents.forEach((c, i) => (c.style.display = i === 0 ? "flex" : "none"));
+} catch (e) {}
+
+// Обратите внимание, что значение block (в двух местах) можно спокойно поменять на flex, если вам это необходимо
